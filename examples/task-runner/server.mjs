e4200f91b2adc4327ccaa7466ad987b2396d0f84
Huynh Duc Dung <dunghd.it@gmail.com>
2024-05-12 21:51:04 +0800

    docs(server.mjs): correct usage instructions and port number in comments

--- examples/task-runner/server.mjs
+++ examples/task-runner/server.mjs
@@ -1,5 +1,5 @@
 // This is example for use task runner for run package.json script
-// Usage `node --run start`
+// Usage: node --run start
 import { createServer } from "node:http";
 
 const server = createServer((req, res) => {
@@ -8,7 +8,7 @@ const server = createServer((req, res) => {
 });
 
 const PORT = process.env.PORT || 3456;
-// starts a simple http server locally on port 3000
+// starts a simple http server locally on port 3456
 server.listen(PORT, "127.0.0.1", () => {
 	console.log(`Listening on http://127.0.0.1:${PORT}`);
 });
